b150ef76e599212f4d47fdfa4263e0053ee0a12b
unknown <masterionic@gmail.com>
2018-06-30 02:49:06 +0800

    nav

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -51,7 +51,7 @@
                 <!-- Logo -->
                 <!-- ============================================================== -->
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="{{url('admin/')}}">
                         <!-- Logo icon -->
                         <b>
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
