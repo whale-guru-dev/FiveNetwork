792345adb445e374dfa1a4c6177338c750dc09e3
unknown <masterionic@gmail.com>
2018-06-29 22:09:31 +0800

    long

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -180,6 +180,7 @@ class LoginController extends Controller
         );
         if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
             $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+            var_dump($ipdat->geoplugin_latitude);exit;
             if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
                 switch ($purpose) {
                     case "location":
