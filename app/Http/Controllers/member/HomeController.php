7836cf569edfa0a7e701cab38981e73f6f75d091
unknown <masterionic@gmail.com>
2018-07-09 22:00:32 +0800

    map

--- app/Http/Controllers/member/HomeController.php
+++ app/Http/Controllers/member/HomeController.php
@@ -19,6 +19,18 @@ class HomeController extends Controller
     public function index()
     {
         $logins = MemberLogin::where('usid', Auth::user()->id)->orderBy('created_at','DESC')->get();
+        $members = User::all();
+        $map_markers = [];
+        foreach($members as $member){
+            $login = MemberLogin::where('usid', $member->id)->orderBy('created_at','DESC')->first();
+            if($login){
+                $map_markers['lat'] = $login->lat;
+                $map_markers['long'] = $login->long;
+                $map_markers['fName'] = $login->member->fName;
+                $map_markers['lName'] = $login->member->lName;
+            }
+            
+        }
         $map_markers = MemberLogin::where('is_active', 1)->get();
     	return view('pages.member.dashbaord')->with(['logins' => $logins,'markers' => $map_markers]);
     }
--- resources/views/pages/landing/landing.blade.php
+++ resources/views/pages/landing/landing.blade.php
@@ -31,7 +31,9 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="{{asset('logo.png')}}" width="250"></a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+          <img src="{{asset('logo.png')}}"  id="logo-img">
+        </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
