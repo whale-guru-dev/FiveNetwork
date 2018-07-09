a36d5415ed650bd4da90a12ba3a7ef924326fa0d
unknown <masterionic@gmail.com>
2018-07-09 09:59:04 +0800

    admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -93,7 +93,23 @@
                     <!-- ============================================================== -->
                     <!-- toggle and nav items -->
                     <!-- ============================================================== -->
-
+                    <ul class="navbar-nav mr-auto mt-md-0">
+                        <!-- This is  -->
+                        <li class="nav-item"> 
+                            <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="mdi mdi-menu"></i>
+                            </a> 
+                        </li>
+                        <li class="nav-item m-l-10"> 
+                            <a class="nav-link sidebartoggler hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i>
+                            </a> 
+                        </li>
+                        <!-- ============================================================== -->
+                        <!-- Comment -->
+                        <!-- ============================================================== -->
+                        <!-- ============================================================== -->
+                        <!-- End Messages -->
+                        <!-- ============================================================== -->
+                    </ul>
                     <!-- ============================================================== -->
                     <!-- User profile and search -->
                     <!-- ============================================================== -->
@@ -101,7 +117,6 @@
                         <!-- ============================================================== -->
                         <!-- Search -->
                         <!-- ============================================================== -->
-
 
                         <!-- ============================================================== -->
                         <!-- Profile -->
@@ -118,18 +133,18 @@
                                                 <img src="{{asset('assets/dashboard/images/users/1.jpg')}}" alt="user">
                                             </div>
                                             <div class="u-text">
-                                                <h4>Steave Jobs</h4>
-                                                <p class="text-muted">varun@gmail.com</p>
+                                                <h4>{{Auth::user()->fName.' '.AUth::user()->lName}}</h4>
+                                                <p class="text-muted">{{Auth::user()->email}}</p>
                                                 <a href="pages-profile.html" class="btn btn-rounded btn-danger btn-sm">View Profile</a>
                                             </div>
                                         </div>
                                     </li>
                                     <li role="separator" class="divider"></li>
                                     <li><a href="#"><i class="ti-user"></i> My Profile</a></li>
-                                    <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li>
-                                    <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
+                                    <!-- <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li> -->
+                                    <!-- <li><a href="#"><i class="ti-email"></i> Inbox</a></li> -->
+                                    <!-- <li role="separator" class="divider"></li> -->
+                                    <!-- <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li> -->
                                     <li role="separator" class="divider"></li>
                                     <li><a href="{{url('admin/logout')}}"><i class="fa fa-power-off"></i> Logout</a></li>
                                 </ul>
